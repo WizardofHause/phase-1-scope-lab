20cda96489ed010ddceb3f7b8d80dd7709b2743e
Jon Hause <jonrhause@gmail.com>
2022-09-21 19:35:19 -0600

    commit tree 3a161ca4edae9225e02f28c88e04333929fe6b32

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase(); //re-defines the variable 'customerName' *return customerName.toUpperCase()* returns 'BOB but does not redefine customerName. 
+}
+
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'karen'; 
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'melody';
+}
